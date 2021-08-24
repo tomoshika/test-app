b3a28ca182d03e23aadcb28cf9e8485a79e6b820
tomoshika <tmm.orange.skm@gmail.com>
2021-08-24 12:50:56 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,35 @@
 class BooksController < ApplicationController
-  
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+
   def create
     book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
   end
 
-  def index
-    @new_book = Book.new
-    @books = Book.all
-    @book = Book.find(params[:id])
-  end
-  
+
   def show
+    @book = Book.find(params[:id])
+
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
-  
+
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title,:body)
   end
